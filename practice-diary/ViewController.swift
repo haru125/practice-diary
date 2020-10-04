15f4cd76b0cc3d3364c1bbb2513695b929b6fba9
haru125 <jhonathan1471@gmail.com>
2020-10-04 20:33:02 +0900

    first commit

---
+++ practice-diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice-diary
+//
+//  Created by 太田都寿 on 2020/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
